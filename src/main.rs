7f99797be2128015fd5431f8d8e8852455d5e781
Ricky Pinder <pinderrr@outlook.com>
2021-09-11 14:44:18 +0100

    projectile output pic

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,40 @@ use raytracer::utils::*;
 use raytracer::tuple::*;
 use raytracer::canvas::*;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let start = Tuple::point(0.0, 1.0, 0.0);
+    let velocity = Tuple::vector(1.0, 1.8, 0.0).normalize() * 11.25;
+    let mut p = Projectile { position: start, velocity }; 
+
+    let gravity = Tuple::vector(0.0, -0.1, 0.0);
+    let wind = Tuple::vector(-0.01, 0.0, 0.0);
+    let e = Environment { gravity, wind };
+
+    let mut c = Canvas::new(900, 550);
+    let red = Color::new(1.0, 0.0, 0.0);
+
+    while p.position.y > 0.0 {
+        let x = p.position.x.round() as usize;
+        let y = (c.height - (p.position.y.round() as u32)) as usize;
+
+        c.write_pixel(x, y, red);
+        p = tick(&e, p);
+    }
+
+    let path = Path::new("output.ppm");
+    let mut file = match File::create(&path) {
+        Err(e) => panic!("couldn't create file: {}", e),
+        Ok(file) => file,
+    };
+
+    match file.write_all(c.to_ppm().as_bytes()) {
+        Err(e) => panic!("couldn't write file: {}", e),
+        Ok(_) => println!("written file"),
+    }
 }
 
 struct Projectile {
@@ -17,7 +49,7 @@ struct Environment {
     wind: Tuple,
 }
 
-fn tick(env: Environment, proj: Projectile) -> Projectile {
+fn tick(env: &Environment, proj: Projectile) -> Projectile {
     let pos = proj.position + proj.velocity;
     let vel = proj.velocity + env.gravity + env.wind;
     Projectile { position: pos, velocity: vel }
